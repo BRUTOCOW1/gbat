17ac88e9869f8bd772ef1d004d9966559e47cd14
Ben Brutocao <ben.brutocao@gmail.com>
2024-08-27 20:17:59 -0500

    big stash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ChildrenOutletContexts } from '@angular/router';
 
+import { createClient } from '@supabase/supabase-js'
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'BenBrutocao';
+
+  data: any;
+  ngOnInit() : void {
+      try {
+          this.fetchData().then((a)=>
+          {
+            this.data = a;
+          });
+        } catch (error) {
+          console.error('Error fetching data:', error);
+          // Handle error as needed
+        }
+        return;
+    }
+
+  async fetchData(){
+      const supabase = createClient("https://pnrjiudqwzklmvdxmlxz.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBucmppdWRxd3prbG12ZHhtbHh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAwNTQ1NjYsImV4cCI6MjAzNTYzMDU2Nn0.PjnBMGwSPqaW6Shez6xusYJfnKARVX7zstxrG71k7m4");
+      let { data: entries, error } = await supabase.from('post').select('*');
+      if (error) {
+          throw new Error(`Error fetching entries: ${error.message}`);
+      }
+      return entries;
+
+  }
 }
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { Router } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AppComponent
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  // Diagnostic only: inspect router configuration
+  constructor(router: Router) {
+    // Use a custom replacer to display function names in the route configs
+    // const replacer = (key, value) => (typeof value === 'function') ? value.name : value;
+
+    // console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+  }
+}
